91d0062fa7b924c9c39a9e88bfdeb35170e31971
bittikatti <28701834+bittikatti@users.noreply.github.com>
2026-03-05 15:33:09 +0200

    Add tooltips to cats and remove count button

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,15 @@ import ninjaCat from './assets/ninjaCat.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div class="cat-container">
-        <img src={catRoll} className="cat spin" alt="Cat sleeping in a roll" />
+        <img src={catRoll} className="cat spin" alt="Cat sleeping in a roll" title="God is sleeping. Do not touch."/>
       </div>
       <h1>Meow World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div class="cat-container">
-        <img src={ninjaCat} className="cat horizontal-movement" alt="Cat sleeping in a roll" />
+      <div class="cat-container" title="Cat is angry. Do not touch.">
+        <img src={ninjaCat} className="cat horizontal-movement" alt="Cat in ninja position and claws out moving back and forth with one leg pointing forward like doing a ninja kick."/>
       </div>
     </>
   )
