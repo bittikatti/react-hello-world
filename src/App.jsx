17a6e2ac466803734bd6466626671357fea67030
bittikatti <28701834+bittikatti@users.noreply.github.com>
2026-03-06 15:22:44 +0200

    Add sun and cloud to the page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,17 @@ function App() {
           <img src={catRoll} className="cat" alt="Cat sleeping in a roll" title="God is sleeping. Do not touch."/>
         </button>
       </div>
+      <div className='weather-container'>
+        {/* Container for sky effects */}
+        {/* TODO Find an effective way to convey effects inside to screen reader. */}
+        <div className='sun sun-glow'></div>
+        <div className='cloud-container horizontal-left-to-right-movement'>
+          {/* https://icons.getbootstrap.com/icons/cloud-fill/ */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white" class="bi bi-cloud-fill" viewBox="0 0 16 16">
+            <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>
+          </svg>
+        </div>
+      </div>
       <h1>Meow World!</h1>
       <div className="cat-container">
         <button className="cat-button horizontal-movement">
